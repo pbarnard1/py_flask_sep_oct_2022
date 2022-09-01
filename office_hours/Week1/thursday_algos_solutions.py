8ff5dfc80638eb6f716b3d98a3746583f408300e
unknown <pabarnard@outlook.com>
2022-09-01 16:09:24 -0700

    Added thursday algos

---
+++ office_hours/Week1/thursday_algos_solutions.py
@@ -0,0 +1,30 @@
+"""
+Find minimum value of list:
+Write a function that accepts a list as input and returns the smallest value
+found.  If the list is empty, return None.
+"""
+
+"""
+Count vowels:
+Write a function that accepts a string and returns the number of vowels found.
+For this exercise, "a", "e", "i", "o" and "u" are vowels - we will not count
+"y" as a vowel.
+
+BONUS: Make the function count "A", "E", etc.  In other words, have this function
+handle all strings, regardless of case sensitivity.
+"""
+
+
+"""
+Factorial:
+In mathematics, n! (read "n factorial") is equal to 1 * 2 * 3 * ... * n.
+For example:
+
+2! = 1 * 2 = 2
+3! = 1 * 2 * 3 = 6
+4! = 1 * 2 * 3 * 4 = 24
+
+Write a function that takes an integer, n, as input and returns the product
+1 * 2 * ... * n.  By definition, 0! = 1 and 1! = 1.  If the number
+is negative, return 1.
+"""
