95b52b2805c67085b92c7853980669e37d6d6e97
unknown <pabarnard@outlook.com>
2022-09-08 14:49:41 -0700

    Added Thursday algo challenges

---
+++ office_hours/Week2/thursday_algos.py
@@ -0,0 +1,32 @@
+"""
+Scrabble Score
+(From https://edabit.com/challenge/cH5ce3f4QgnreDW4v)
+You are given a list of dictionaries, where each dictionary looks like the following:
+{"tile": "A", "score": 1}
+Your objective is to return the total score from the list of tiles (dictionaries).
+
+Example:
+x = [
+    {"tile": "P", "score": 3},
+    {"tile": "H", "score": 4},
+    {"tile": "A", "score": 1},
+    {"tile": "N", "score": 1},
+    {"tile": "T", "score": 1},
+    {"tile": "O", "score": 1},
+    {"tile": "M", "score": 3},
+]
+PHANTOM will return 14 (3 + 4 + 1 + 1 + 1 + 1 + 3).
+"""
+
+
+
+"""
+Filter Strings from Array
+(Modified from this version from Edabit: https://edabit.com/challenge/nunJurLEibCyn8fzn)
+Given a list containing strings and numbers, return a new list with all string values removed.
+Keep the original order of the values the same.
+
+Examples: 
+[3, 8, "hello", -4] should return [3, 8, -4]
+["money", "33", 4, "blue", 5] should return [4, 5] ("33" is a string, not a number)
+"""
