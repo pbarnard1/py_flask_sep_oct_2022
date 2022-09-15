cead8d30ee1e5777344589fa7344a4021fdb8527
unknown <pabarnard@outlook.com>
2022-09-15 15:35:55 -0700

    Added algorithm challenges for September 15, 2022

---
+++ office_hours/Week3/thursday_algos_solutions.py
@@ -0,0 +1,26 @@
+# Week 3: Frequency Distribution (https://edabit.com/challenge/KKmM4ob5wwPwf8kgS)
+"""
+Frequency distribution (from https://edabit.com/challenge/KKmM4ob5wwPwf8kgS):
+
+Given a list of items, return a dictionary that contains the frequency of each item.
+Each key will be an entry from the list, and the value linked to the key will be
+the number of occurrences of said key in the list.  If the list is empty, return
+an empty dictionary.
+
+Examples:
+["M", "P", "C", "C", "P"] returns { "M": 1, "P": 2, "C": 2} as there are 2 "P"s, 2 "C"s and 1 "M"
+[1, 5, 8, 5, 5, 3] returns { 1: 1, 5: 3, 8: 1, 3: 1 } (Note it's okay that a key is a number or boolean)
+[] returns {}
+"""
+
+# Number of digits in a number (https://edabit.com/challenge/yFJzLfYghz7ZtsyAN)
+"""
+Given an integer as input, return the number of digits in the integer.
+You may assume the number is an integer (whole number) only.
+
+Examples:
+24 -> 2 digits
+23849 -> 5
+0 -> 1
+-15 -> 2 (don't count the "-")
+"""
